bcec09520a1f284e8e4fbafaeac57514cc335ea7
DxniChid <chidiroglou.danai06@gmail.com>
2026-02-11 15:54:53 +0100

    Style added & Button made
    Style not done, just done roughly

--- src/App.vue
+++ src/App.vue
@@ -55,6 +55,7 @@ function addItem() {
 
   
   </div>
+    <button class="add">+</button>
 </template>
 
 <style scoped>
@@ -62,8 +63,93 @@ function addItem() {
 .body {
   min-height: 1000px;
   background: #f4f6f8;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 16px;
+}
+
+.settings {
+  font-size: 40px;
+  color: #A8D3E9;
+  cursor: pointer;
+}
+
+.logo-img {
+  height: 80px;
+  margin-left: 42%;
+  padding: 50px;
+}
+
+.welcome {
+  padding: 16px;
+  margin-left: 46%;
+}
+
+.card {
+  background: #D5E8F2;
+  margin-left: 37%;
+  padding: 16px;
+  width: 900px;
+  border-radius: 12px;
+  box-shadow: 0 4px 10px rgba(0,0,0,0.05);
+  font-size: 20px;
+}
+
+.timeline-row {
+  display: flex;
+  align-items: flex-start;
+  gap: 12px;
+  margin-bottom: 12px;
 
 }
 
+.timeline-left {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
+.dot {
+  width: 10px;
+  height: 10px;
+  background: #8AB3C2 ;
+  border-radius: 50%;
+}
+
+.line {
+  width: 2px;
+  height: 30px;
+  background: #ddd;
+  margin-top: 2px;
+}
+
+.time {
+  font-weight: bold;
+  width: 60px;
+}
+
+.task {
+  flex: 1;
+}
+
+
+.add {
+  background-color: #8AB3C2;
+  border-color: #D5E8F2;
+  margin-left: 50%;
+  margin-top: 500px;
+  transform: translateX(-50%);
+  width: 70px;
+  height: 70px;
+  font-size: 50px;
+  color: #FFFFFF;
+  border-radius: 50%;
+  border: none;
+
+
+}
 </style>
