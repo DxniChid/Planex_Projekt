fc7e21e2bdfd7ac14c4b16e03c49e813429c87c3
DxniChid <chidiroglou.danai06@gmail.com>
2026-02-11 15:08:11 +0100

    Homepage
    Needs to be fixed & isnt done yet

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,71 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue"
+import logo from "@/assets/logo.png"
+
+const name =  ref("Max Mustermann");
+
+const todayItems = ref([
+
+{ time: "06:00", task: "Hund Gassi" },
+{ time: "11:30", task: "Einkaufen" }
+])
+
+function addItem() {
+  todayItems.value.push({
+    time: "00:00",
+    task: "Neue Aufgabe"
+  })
+}
+
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<header class="header">
+      <div class="menu">☰</div>
+
+      <img
+        :src="/assets/logo.png"
+        alt="Planex Logo"
+        class="logo-img"
+      />
+    </header>
+        <div class="welcome">
+      <h1>Willkommen</h1>
+      <h2>{{ name }}</h2>
+    </div>
+
+      <div class="card">
+      <h3 class="card-title">Heute</h3>
+
+      <div class="timeline">
+        <div
+          v-for="(item, i) in todayItems"
+          :key="i"
+          class="timeline-row"
+        >
+          <div class="timeline-left">
+            <div class="dot"></div>
+            <div v-if="i !== todayItems.length - 1" class="line"></div>
+          </div>
+
+          <div class="time">{{ item.time }}</div>
+          <div class="task">{{ item.task }}</div>
+        </div>
+      </div>
+    </div>
+
+ 
+
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.body {
+  min-height: 1000px;
+  background: #f4f6f8;
+
+}
+
+
+</style>
