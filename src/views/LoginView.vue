599e8777b1e56b37e56fe9afb0440913ea664ff5
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-02-25 16:43:23 +0100

    You can now log in, and i created an method, where it checks the future pinia database for an user like this.

---
+++ src/views/LoginView.vue
@@ -0,0 +1,52 @@
+<script >
+
+
+export default{
+    
+
+    data(){
+        return {
+            name: null,
+            email: null,
+            submit: false
+        }
+    },
+    methods: {
+        checkAcc(name, email){
+            if(true){
+                alert("Hello " + name + "your email: " + email)
+                this.submit = true
+                this.$router.push("/")
+                
+            }
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div id="login">
+        <form v-on:submit.prevent="checkAcc(name, email)">
+            <label for="name">Username</label> <input type="text" name="name" v-model="name">
+            <br>
+            <label for="email">email</label> <input type="text" name="name" v-model="email">
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+    #login{
+        display: flex;
+        justify-content: center;   
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        flex-shrink: 2;
+        gap: 5px;
+        background-color: aquamarine;
+        padding: 20px;
+    }
+</style>
