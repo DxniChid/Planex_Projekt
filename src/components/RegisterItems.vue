8788808675bd09cf95befed4b7834be5bfae9823
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-03-04 16:24:48 +0100

    Component & PiniaStore
    Made the Component for the registery and created for the Userdata a Pinia Store. It works just fine

---
+++ src/components/RegisterItems.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { useAuthStore } from '@/stores/useAuthStore';
+
+const userData = useAuthStore()
+
+
+</script>
+
+<template>
+<form v-on:submit.prevent="console.log(userData.prename)">
+            <div id="input"><label for="name">Vorname*</label> <br><input type="text" v-model="userData.prename"></div>
+            <br>
+            <div id="input"><label for="name">Nachname</label> <br><input type="text" name="name"></div>
+            <br>
+            <div id="input"><label for="name">Email*</label> <br><input type="email" name="name"></div>
+            <br>
+            <div id="input"><label for="name">Passwort*</label> <br><input type="password" name="name"></div>
+            <br>
+            <div id="input"><label for="name">Passwort bestätigen*</label> <br><input type="password" name="name"></div>
+            <br>
+            <button type="submit">Registrieren</button>
+        </form> 
+</template>
+
+<style scoped>
+
+    form {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    padding: 20px;
+    /* Add this line */
+    align-items: center; 
+    }
+    input {
+        width: 450px;
+        height: 25px;
+    }
+    #input {
+        margin-bottom: 10px;
+        margin-top: 10px;
+    }
+    button {
+        margin-top: 30px;
+        width: 180px;
+        font-size: 30px;
+    }
+    label {
+        font-size: 30px;
+    }
+
+</style>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,19 +1,8 @@
-<script >
 
+<script >
+import RegisterItems from'@/components/RegisterItems.vue'
 
 export default{
-    
-
-    data(){
-        return {
-            prename: null,
-            name: null,
-            email: null,
-            password: null,
-            confirmation: null,
-            submit: false
-        }
-    },
     methods: {
         checkAcc(prename, email){
             if(true){
@@ -23,6 +12,9 @@ export default{
                 
             }
         }
+    },
+    components: {
+        RegisterItems
     }
 }
 
@@ -32,19 +24,7 @@ export default{
     <img id="logo" src="@/assets/logo.png" alt="">
     <h1>Registierung</h1>
     <div id="login">
-        <form v-on:submit.prevent="checkAcc(name, email)">
-            <div id="input"><label for="name">Vorname*</label> <br><input type="text" name="name" v-model="prename"></div>
-            <br>
-            <div id="input"><label for="name">Nachname</label> <br><input type="text" name="name" v-model="name"></div>
-            <br>
-            <div id="input"><label for="name">Email*</label> <br><input type="email" name="name" v-model="email"></div>
-            <br>
-            <div id="input"><label for="name">Passwort*</label> <br><input type="password" name="name" v-model="password"></div>
-            <br>
-            <div id="input"><label for="name">Passwort bestätigen*</label> <br><input type="password" name="name" v-model="confirmation"></div>
-            <br>
-            <button type="submit">Registrieren</button>
-        </form> 
+        <RegisterItems/>
     </div>
     <p>Bereits ein Konto? Klicken Sie &nbsp; <RouterLink to="/login"> <u style="cursor: pointer;" >Hier</u></RouterLink></p>
 </template>
@@ -56,14 +36,7 @@ export default{
         display: flex;
         justify-content: center;   
     }
- form {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-    padding: 20px;
-    /* Add this line */
-    align-items: center; 
-}
+
     h1, p {
         display: flex;
         justify-content: center;
@@ -73,22 +46,7 @@ export default{
         font-size: 60px;
         font-weight: 400;
     }
-    input {
-        width: 450px;
-        height: 25px;
-    }
-    #input {
-        margin-bottom: 10px;
-        margin-top: 10px;
-    }
-    button {
-        margin-top: 30px;
-        width: 180px;
-        font-size: 30px;
-    }
-    label {
-        font-size: 30px;
-    }
+
     img {
         display: block;    /* Required for margin: auto to work */
         margin-left: auto;
