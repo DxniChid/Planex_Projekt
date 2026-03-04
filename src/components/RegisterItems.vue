a5adf078159d639e893b2188327c795dc687a5fb
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-03-04 16:57:07 +0100

    WIP-Pinia
    created some actions in userDataStroe

--- src/components/RegisterItems.vue
+++ src/components/RegisterItems.vue
@@ -7,19 +7,20 @@ const userData = useAuthStore()
 </script>
 
 <template>
-<form v-on:submit.prevent="console.log(userData.prename)">
+<form v-on:submit.prevent="userData.checkPass(userData.password, userData.confirmation, userData.prename, userData.name, userData.email)">
             <div id="input"><label for="name">Vorname*</label> <br><input type="text" v-model="userData.prename"></div>
             <br>
-            <div id="input"><label for="name">Nachname</label> <br><input type="text" name="name"></div>
+            <div id="input"><label for="name">Nachname</label> <br><input type="text" v-model="userData.name"></div>
             <br>
-            <div id="input"><label for="name">Email*</label> <br><input type="email" name="name"></div>
+            <div id="input"><label for="name">Email*</label> <br><input type="email" v-model="userData.email"></div>
             <br>
-            <div id="input"><label for="name">Passwort*</label> <br><input type="password" name="name"></div>
+            <div id="input"><label for="name">Passwort*</label> <br><input type="password" v-model="userData.password"></div>
             <br>
-            <div id="input"><label for="name">Passwort bestätigen*</label> <br><input type="password" name="name"></div>
+            <div id="input"><label for="name">Passwort bestätigen*</label> <br><input type="password"v-model="userData.confirmation"></div>
             <br>
             <button type="submit">Registrieren</button>
         </form> 
+        <button @click="userData.printInitials">Press me</button>
 </template>
 
 <style scoped>
--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -2,11 +2,37 @@ import { ref, computed } from 'vue'
 import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore ('auth', {
-        state: () => ({
-            name: 'Hans Jürgen Peterson',
-            prename: '',
-            email: '',
-            password: '',
-            confirmation: ''
-           })
-        })
+    state: () => ({
+        name: '',
+        prename: '',
+        email: '',
+        password: '',
+        confirmation: ''
+    }),
+    actions: {
+        checkPass(password, confirmation, prename, name, email) {
+            if(password !== confirmation || password == null){
+                return alert("Passwords do not match")
+            }
+            console.log("YEAH MAN")
+            this.insertData(prename, name, email, password, confirmation)
+        }, 
+        printInitials(){
+            alert(
+                "Prename: " + this.prename +
+                "Name: " + this.name +
+                "Email: " + this.email +
+                "Password: " + this.password +
+                "Confirmation: " + this.confirmation
+            )
+        },
+        insertData(prename, name, email, password, confirmation){
+            this.prename = prename,
+            this.name = name,
+            this.email = email,
+            this.password = password,
+            this.confirmation = confirmation
+        }
+    },
+    persist: true
+})
